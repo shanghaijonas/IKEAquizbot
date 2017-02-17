e129082236045ec595540ef7f9b91a215c775d82
shanghaijonas <jonas.karlsson@vibe.se>
2017-02-17 15:07:12 +0800

    Score and design changes
    Added score based on time

--- messages/index.js
+++ messages/index.js
@@ -3,9 +3,10 @@
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
-//var restify = require('node_modules/restify');
 
 var useEmulator = (process.env.NODE_ENV == 'development');
+useEmulator=true;
+console.log('Start with emulator: %s', useEmulator);
 
 var ikeaproducts = [
     { name: "BREIM", image: "http://www.ikea.com/PIAimages/0291703_PE424963_S3.JPG"}, //http://www.ikea.com/cn/en/images/products/breim-wardrobe-blue__0291705_PE424964_S4.JPG
@@ -23,8 +24,16 @@ var ikeaproducts = [
 ];
 
 var productindex;
-var score=0;
+var score=0, round=0, totalScore=0;
+var startTime, endTime;
 const constNbrAnswers = 3; //Must be lower then the number of products
+
+// Score variables - each question can give you between 500-1000 points depending on how fast you answer
+const constScoreMinPoints = 500;
+const constScoreMaxPoints = 1000;                
+const constScoreMinTime = 2000; // milliseconds
+const constScoreMaxTime = 10000; // millseconds
+
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -36,12 +45,28 @@ function shuffleArray(array) {
     return array;
 }
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
-});
+var connector = useEmulator ? 
+    new builder.ChatConnector({
+        appId: process.env.MICROSOFT_APP_ID,
+        appPassword: process.env.MICROSOFT_APP_PASSWORD
+    }) : 
+    new botbuilder_azure.BotServiceConnector({
+        appId: process.env['MicrosoftAppId'],
+        appPassword: process.env['MicrosoftAppPassword'],
+        stateEndpoint: process.env['BotStateEndpoint'],
+        openIdMetadata: process.env['BotOpenIdMetadata']
+    });
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    module.exports = { default: connector.listen() }
+}
 
 var bot = new builder.UniversalBot(connector);
 
@@ -73,12 +98,10 @@ bot.dialog('/', [
         session.send(msg);
         session.send("Hi... I'm the IKEA gaming robot!");
 
-        // How to get Skype/FB username
-        //session.send('Hello %s!', session.userData.name);
-
-        //var address = JSON.stringify(session.message.address.);
-        //var address = JSON.parse(session.message.address)
-        //session.send(address);
+        // How to get Skype/FB
+        var address = JSON.stringify(session.message.address);
+        var address2 = JSON.parse(address);
+        session.send("MSG: %s", address2["channelId"]);
                 
         session.beginDialog('/menu');
 
@@ -98,6 +121,7 @@ bot.dialog('/menu', [
     function (session, results) {
         switch (results.response.index) {
             case 0:
+                round = 0;
                 session.beginDialog('/Play game');
                 break;
             case 1:
@@ -125,21 +149,49 @@ bot.dialog('/Rules', [
 
 // Play game
 bot.dialog('/Play game', [
+    
     function (session) {
-
+        
         session.sendTyping();
 
         var s = shuffleArray(ikeaproducts);
         var nbrAnswers = constNbrAnswers % s.length;
-        if (score>3) nbrAnswers = (nbrAnswers+1) % s.length;
+
+        round=round+1;
+        if (round>3) nbrAnswers = (nbrAnswers+1) % s.length;
 
         productindex = Math.floor(Math.random() * nbrAnswers);
 
-        var msg = new builder.Message(session)
+        var card = new builder.HeroCard(session)
+            .title("Round " + round + " - What IKEA product is this?")
+            //.text("What IKEA product is this?")
+            .images([
+                 builder.CardImage.create(session, s[productindex].image)
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+
+       /* var msg = new builder.Message(session)
+            //.title('Hello')
+            //.subtitle('Subtitle')
+            //.text('Text')
             .attachments([{
                 contentType: "image/jpeg",
                 contentUrl: s[productindex].image
-            }]);
+            }]);*/
+        
+        /*var msg = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .attachments(new builder.HeroCard(session)
+                    .title('What IKEA furnitures is this?')
+                    .subtitle('Subtitle')
+                    .text('The text')
+                    .images([
+                        builder.CardImage.create(session, s[productindex].image)
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+                    ]));*/
+
         session.send(msg);
 
         var answers=[];
@@ -147,18 +199,28 @@ bot.dialog('/Play game', [
            answers[i]=s[i].name;
         }
 
-       builder.Prompts.choice(session, "What IKEA product is this?", answers, { listStyle: builder.ListStyle.button });
+        startTime = new Date().getTime();
+        //builder.Prompts.choice(session, "Round: " + round + " - What IKEA product is this?", answers, { listStyle: builder.ListStyle.button });        
+        builder.Prompts.choice(session, "", answers, { listStyle: builder.ListStyle.button });        
     },
     function (session, results) {
-         if (results.response && results.response.entity != '(quit)') {
-            session.send("You chose '%s'", results.response.entity);
+         if (results.response && results.response.entity != 'quit') {
+                        
+            session.send("You chose '%s'", results.response.entity);                        
+
             if (results.response.entity==ikeaproducts[productindex].name){
-                score=score+1;
-                session.send("Your answer is correct.\n Your score is now: "+score);
+                endTime = new Date().getTime();
+                
+                // Calculate score
+                score = constScoreMinPoints + Math.floor(((constScoreMaxPoints-constScoreMinPoints) * (1 - ((Math.max(Math.min(endTime-startTime,constScoreMaxTime),constScoreMinTime))-constScoreMinTime) / (constScoreMaxTime-constScoreMinTime))));
+                console.log(score);
+
+                totalScore=totalScore+score;
+                session.send("Your answer is correct.\n Your total score is now: %s.", totalScore);
                 session.replaceDialog('/Play game');
             } else {
-                session.send("Your answer is wrong.\n The name of the product is '"+ikeaproducts[productindex].name+"'. Your score was: "+score);
-                score=0;
+                session.send("Your answer is wrong.\n The name of the product is '"+ikeaproducts[productindex].name+"'. Your score was: "+totalScore);
+                totalScore=0;
                 session.endDialog();
             }
 
@@ -169,15 +231,3 @@ bot.dialog('/Play game', [
     }
 ])
 
-
-
-if (useEmulator) {
-    var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(3978, function() {
-        console.log('test bot endpont at http://localhost:3978/api/messages');
-    });
-    server.post('/api/messages', connector.listen());
-} else {
-    module.exports = { default: connector.listen() }
-}
